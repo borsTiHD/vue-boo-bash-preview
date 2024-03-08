7c4475c6b0d351fe897fbe80c2639d3ef4f51efc
borsTiHD <bastian.j@gmx.net>
2024-03-08 20:10:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { BooBash } from 'vue-boo-bash'
+import 'vue-boo-bash/style.css'
+</script>
+
+<template>
+    <main class="absolute inset-0 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+        <BooBash />
+    </main>
+</template>
