7873e87e7dee3f7797d31dfeb862d5b1ea6cff36
borsTiHD <bastian.j@gmx.net>
2024-03-09 10:04:41 +0100

    feat: 🎨 No need for a ref 😋

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import { BooBash } from 'vue-boo-bash'
 import 'vue-boo-bash/style.css'
 
 // Optional and reactive game settings
 // On change, the game will immediately update
-const settings = ref({
+const settings = {
     maxGameTime: 40 // specify the game time in seconds - default 30 seconds
     // maxGhosts: 10, // specify the maximum number of ghosts - default 10
     // spawnDuration: 2000, // specify the spawn rate for new ghosts - default 2000ms
     // music: undefined, // specify source for background music
     // debug: true
-})
+}
 </script>
 
 <template>
