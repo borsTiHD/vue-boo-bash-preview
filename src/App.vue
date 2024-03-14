adda13162b8d0cd9f02e7a9730ca040e02a3f3b8
borsTiHD <bastian.j@gmx.net>
2024-03-14 10:28:03 +0100

    feat: ✨ Updating to latest game version, also using optional music import

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { BooBash } from 'vue-boo-bash'
 import 'vue-boo-bash/style.css'
+import BackgroundMusic from 'vue-boo-bash/Ghost_House_Orchestral_Cover.mp3'
 
 // Optional and reactive game settings
 // On change, the game will immediately update
 const settings = {
-    maxGameTime: 40 // specify the game time in seconds - default 30 seconds
+    maxGameTime: 40, // specify the game time in seconds - default 30 seconds
     // maxGhosts: 10, // specify the maximum number of ghosts - default 10
     // spawnDuration: 2000, // specify the spawn rate for new ghosts - default 2000ms
-    // music: undefined, // specify source for background music
+    music: BackgroundMusic // specify source for background music
     // debug: true
 }
 </script>
